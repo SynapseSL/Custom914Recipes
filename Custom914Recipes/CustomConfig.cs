191ffb88f54e9df9dc15b0d3f80d594c8a5dbb26
GrafDimenzio <crafterwolfhd@gmail.com>
2021-02-23 09:43:40 +0100

    Projektdateien hinzufügen.

---
+++ Custom914Recipes/CustomConfig.cs
@@ -0,0 +1,11 @@
+﻿using Synapse.Api;
+using System.Collections.Generic;
+using Synapse.Config;
+
+namespace Custom914Recipes
+{
+    public class CustomConfig : IConfigSection
+    {
+        public List<Scp914Recipe> Scp914Recipes { get; set; } = new List<Scp914Recipe>();
+    }
+}
---
+++ Custom914Recipes/PluginClass.cs
@@ -0,0 +1,56 @@
+﻿using Synapse;
+using Synapse.Api;
+using Synapse.Api.Plugin;
+using Synapse.Config;
+using System.IO;
+using System.Linq;
+using MEC;
+
+namespace Custom914Recipes
+{
+    [PluginInformation(
+        Author = "Dimenzio",
+        Description = "A Plugin that allows to configure SCP-914",
+        LoadPriority = 0,
+        Name = "Custom914Recipes",
+        SynapseMajor = 2,
+        SynapseMinor = 5,
+        SynapsePatch = 0,
+        Version = "v.1.0.0"
+        )]
+    public class PluginClass : AbstractPlugin
+    {
+        private SYML SYMLConfig { get; set; }
+
+        private CustomConfig DefaultConfig { get; set; } = new CustomConfig();
+
+        private CustomConfig Config { get; set; }
+
+        public override void Load()
+        {
+            var nonSharedPath = Path.Combine(Server.Get.Files.ConfigDirectory, "SCP914Recipes.syml");
+            var path = File.Exists(nonSharedPath) ? nonSharedPath : Path.Combine(Server.Get.Files.SharedConfigDirectory, "SCP914Recipes.syml");
+
+            SYMLConfig = new SYML(path);
+
+            //Wait 3 Seconds so that the Game has enough time to load the default Values
+            Timing.CallDelayed(3f, () => 
+            {
+                DefaultConfig.Scp914Recipes = Map.Get.Scp914.Recipes.ToList();
+                ReloadConfigs();
+            });
+        }
+
+        public override void ReloadConfigs()
+        {
+            SYMLConfig.Load();
+
+            Config = SYMLConfig.GetOrSetDefault("SCP914Recipes", DefaultConfig);
+
+            Map.Get.Scp914.Recipes.Clear();
+
+            foreach (var recipe in Config.Scp914Recipes)
+                Map.Get.Scp914.Recipes.Add(recipe);
+        }
+    }
+}
